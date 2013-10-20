2fed8cdb21769d32fb3b96f3ca0f08eeb641ffcd
Sonnbc <sonnbc.second@gmail.com>
2013-10-20 15:14:21 -0500

    saturday outage save

--- mp2/sender.py
+++ mp2/sender.py
@@ -2,62 +2,82 @@ import sys
 from socket import *
 from common import *
 import errno
-#------------------------------------------------------------------------------
-last_byte_sent = 0
-last_byte_acked = 0
 
-send_base = INIT_SEQ_NUM
-next_seq_num = INIT_SEQ_NUM
+class TCPSender:
+
+    self.last_byte_sent = 0
+    self.last_byte_acked = 0
+
+    self.send_base = INIT_SEQ_NUM
+    self.next_seq_num = INIT_SEQ_NUM
+
+#TODO: initialize timer and timeout
+    
+    def __init__(receiver_domain, receiver_port):
+        self.connection = self.make_connection(receiver_domain, receiver_port)    
+   
+#------------------------------------------------------------------------------
+    def udp_send(self, segment):
+        sock, domain, port = connection
+        sock.sendto(data, (domain, port))
+#------------------------------------------------------------------------------
+    def udp_receive(self):
+        sock, _, _ = connection
+        try:
+            return sock.recvfrom(MSS)
+        except:
+            return None
+#------------------------------------------------------------------------------
+    def make_connection(self, target_domain, target_port):
+        my_socket = socket(AF_INET, SOCK_DGRAM)
+        my_socket.setblocking(0)
+        return my_socket, target_domain, target_port
+#------------------------------------------------------------------------------    
+    def handle_ack(self, segment):
+        
+        ack = segment.get_ack()
+        if ack <= self.send_base:
+            return
+        
+        acked_id = byte_to_id(ack - 1)
+        
+        self.send_base = ack
+        self.last_byte_acked = ack - 1
+        
+        if last_byte_sent > last_byte_acked:
+            timer = sent_time[acked_id + 1] + timeout
+#------------------------------------------------------------------------------
+    def handle_timeout(self):
+        first_byte_unacked = last_byte_acked + 1
+        first_id_unacked = byte_to_id(first_byte_unacked)
+        transmit(first_id_unacked)
 
 #------------------------------------------------------------------------------
-def udp_send(data, connection):
-    sock, domain, port = connection
-    sock.sendto(data, (domain, port))
-#------------------------------------------------------------------------------
-def udp_receive(connection):
-    sock, _, _ = connection
-    try:
-        return sock.recvfrom(MSS)
-    except:
-        return None
-#------------------------------------------------------------------------------
-def make_connection(target_domain, target_port):
-    my_socket = socket(AF_INET, SOCK_DGRAM)
-    my_socket.setblocking(0)
-    return my_socket, target_domain, target_port
+    def transmit_segment(self, idx):
+        segment = self.build_segment(idx) #header + data[idx]
+        self.udp_send(segment)
+        
+        last_byte_sent = id_to_max_byte(idx)
+        
+               
 #------------------------------------------------------------------------------    
-def handle_ack(message):
-    global send_base
-    
-    ack = message.get_ack()
-    if ack <= send_base:
-        return
-    
-    acked_id = ack_to_id(ack)
-    
-    send_base = ack
-    last_byte_acked = ack - 1
-    
-    if last_byte_sent > last_byte_acked:
-        timer = sent_time[acked_id + 1] + timeout
-           
-#------------------------------------------------------------------------------    
-def process(data, connection):
-    while True:
-        ack = udp_receive(connection)
-        if ack:
-            handle_ack(ack)
-        elif current_time() >= timer:
-            handle_timeout()
-        elif has_data()
-            send()  
+    def run(self, data):
+        self.data = data
+        while True:
+            ack = udp_receive(connection)
+            if ack:
+                handle_ack(ack)
+            elif current_time() >= timer:
+                handle_timeout()
+            elif has_data()
+                send()  
 #------------------------------------------------------------------------------    
 def main(filename, receiver_domain, receiver_port):
-    connection = make_connection(receiver_domain, receiver_port)
-    udp_receive(connection)
+    sender = TCPSender(receiver_domain, receiver_port)
+
     with open(filename) as my_file:
         data = list(iter(lambda: my_file.read(MSS), ''))
-    process(data, connection)
+    sender.run(data)
 #------------------------------------------------------------------------------    
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
