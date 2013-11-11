cc8aaf70f6e718bd710ddd3fda6be0814f7754b5
Md Tanvir Amin <tanviralamin@gmail.com>
2013-11-11 17:06:40 -0600

    added test file generation

--- mp2/test.py
+++ mp2/test.py
@@ -1,3 +1,20 @@
-lst = [str(i) for i in xrange(0, 10000000)]
-print ','.join(lst)
+import sys
 
+def generate(size):
+  x = 1
+  sum = 0
+  while 1:
+    print x
+    sum += len(str(x)) + 1
+    if sum >= size:
+      return sum
+    x += 1
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print >> sys.stderr, 'Usage: python test.py size-in-bytes'
+    sys.exit(1)
+  size = int(sys.argv[1])
+  sum = generate(size)
+  print >> sys.stderr, sum , 'bytes generated'
+  sys.exit(0)
