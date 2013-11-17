a48776c8a8aadb84317ca74b31b7eb82b0abdbdb
Tanvir Amin <tanviralamin@gmail.com>
2013-11-17 00:39:42 -0600

    plot results check for file

--- mp2/plot-results.sh
+++ mp2/plot-results.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -f cwnd ];
+then
 ./plot.sh cwnd pdf 1 2 "0.2" "cwnd vs Time" "Time (ms)" "cwnd (bytes)" $1 $2 
+fi
+
+if [ -f trace ];
+then
 ./plot.sh trace pdf 3 2 "0.2" "Sequence Number vs Time" "Time (ms)" "Sequence Number" $1 $2
+fi
+
 
