0ab85515f839c568392503e1192fa24c3c4ed329
Tanvir Amin <tanviralamin@gmail.com>
2013-11-17 02:01:01 -0600

    readme

--- mp2/plot-results.sh
+++ mp2/plot-results.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ -f cwnd ];
 then
-./plot.sh cwnd pdf 1 2 "0.2" "cwnd vs Time" "Time (ms)" "cwnd (bytes)" $1 $2 
+./plot.sh cwnd eps 1 2 "0.2" "cwnd vs Time" "Time (ms)" "cwnd (bytes)" $1 $2 
 fi
 
 if [ -f trace ];
 then
-./plot.sh trace pdf 3 2 "0.2" "Sequence Number vs Time" "Time (ms)" "Sequence Number" $1 $2
+./plot.sh trace eps 3 2 "0.2" "Sequence Number vs Time" "Time (ms)" "Sequence Number" $1 $2
 fi
 
 
